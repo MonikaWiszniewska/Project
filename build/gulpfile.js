becd1f3eb61fcdc545b7bf8171352069d6d119ee
Monika Wiszniewska <nikawisz@gmail.com>
2016-04-26 17:12:09 +0200

    server

---
+++ build/gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+gulp.task('copy_task', function() {
+    gulp.src('*.html')
+    .pipe(gulp.dest('../build/'));
+    gulp.src('*.js')
+    .pipe(gulp.dest('../build/'));
+    gulp.src('*.json')
+    .pipe(gulp.dest('../build/'));
+    gulp.src('css/*.css')
+    .pipe(gulp.dest('../build/css'));
+    gulp.src('data/*.js')
+    .pipe(gulp.dest('../build/data'));
+    gulp.src('js/*.js')
+    .pipe(gulp.dest('../build/js'));
+    gulp.src('lib/*.js')
+    .pipe(gulp.dest('../build/lib'));
+    gulp.src('partials/*.html')
+    .pipe(gulp.dest('../build/partials'));
+});
+
+gulp.task('build:dev', function() {
+    gulp.src('*.html')
+    .pipe(gulp.dest('../build/'));
+    gulp.src('*.js')
+    .pipe(gulp.dest('../build/'));
+    gulp.src('*.json')
+    .pipe(gulp.dest('../build/'));
+    gulp.src('css/*.css')
+    .pipe(gulp.dest('../build/css'));
+    gulp.src('js/*.js')
+    .pipe(gulp.dest('../build/js'));
+    gulp.src('lib/*.js')
+    .pipe(gulp.dest('../build/lib'));
+});
+
+gulp.task('build:prod', function() {
+    gulp.src('*.html')
+    .pipe(gulp.dest('../build/'));
+    gulp.src('*.js')
+    .pipe(gulp.dest('../build/'));
+    gulp.src('*.json')
+    .pipe(gulp.dest('../build/'));
+    gulp.src('css/*.css')
+    .pipe(gulp.dest('../build/css'));
+    gulp.src('js/*.js')
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('../build/js'));
+    gulp.src('lib/*.js')
+    .pipe(gulp.dest('../build/lib'));
+});
