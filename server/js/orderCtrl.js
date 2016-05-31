7d4a4e4727db948befcc7bf2dfa5c5e3883281a8
Monika Wiszniewska <nikawisz@gmail.com>
2016-05-31 12:13:42 +0200

    order

--- server/js/orderCtrl.js
+++ server/js/orderCtrl.js
@@ -1,12 +1,18 @@
-angular.module('myapp').controller('OrderCtrl', function ($scope, menu, ingredients) {
+angular.module('myapp').controller('OrderCtrl', function ($state, $scope, menu, ingredients) {
     menu.getItems().then(function (data) {
-       $scope.Menu = data.data; 
-    });
+       $scope.Menu = data.data; });
+       $scope.submitForm = function(form){
+        if(form.$valid) {
+            console.log($scope.phone);
+             console.log($scope.street);
+             console.log($scope.remarks);
+        }
+       }
+       $scope.back = function(){
+           $state.go('menu');
+       }
     
     ingredients.getIngredients().then(function(data) {
     	$scope.Ingredients = data.data;
-    });
-    
-    
-    
-})
+    }) 
+});
