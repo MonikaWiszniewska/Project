e3fe6d22a6ca683bd00b6f4759a637d7ccb1b07d
Monika Wiszniewska <nikawisz@gmail.com>
2016-06-02 20:27:41 +0200

    test

--- server/js/contactCtrl.js
+++ server/js/contactCtrl.js
@@ -2,4 +2,5 @@ angular.module('myapp').controller('ContactCtrl', function ($scope, contact) {
     contact.getContact().then(function (data) {
        $scope.Contact = data.data; 
     });
+    
 })
--- server/js/orderCtrl.js
+++ server/js/orderCtrl.js
@@ -1,22 +1,49 @@
 angular.module('myapp').controller('OrderCtrl', function ($state, $scope, menu, ingredients, order) {
-    $scope.orderData = {};
-    menu.getItems().then(function (data) {
-       $scope.Menu = data.data; });
-       
-       $scope.submitForm = function(form){
-        if(form.$valid) {
-            order.sendOrder($scope.orderData).then(function (data) {
-               console.log(data); 
-            $scope.id = data.data; 
+        
+        menu.getItems().then(function (data) {
+        $scope.Menu = data.data; });
+        
+        ingredients.getIngredients().then(function(data) {
+            $scope.Ingredients = data.data;
+        });
+        
+
+        $scope.orderInfo = {};
+        var basket = Array(sessionStorage.cart);
+         var orderData = {
+            order: [],
+            extras: [],
+            orderInfo: {}
+        };
+
+         basket.forEach(function(element){
+                 console.log(element);
+                    orderData.order.push({
+                            id: element.id,
+                            quantity: element.count
+                        });
+        });
+
+
+        orderData.orderInfo = $scope.orderInfo;
+        $scope.Test = orderData;
+         
+         
+        $scope.submitForm = function(form){
+            if(form.$valid) {
+                order.sendOrder(orderData).then(function (data) {
+                console.log(data); 
+                $scope.id = data.data.id; 
+                
+                //console.log($scope.id);
+            }, function(error) {
+            $scope.errorMessage = "Błąd wysyłania " + error.message;
             });
-            console.log($scope.id);
-        }
-       }
-       $scope.back = function(){
-           $state.go('menu');
-       }
+          }
+        };
+        
+        $scope.back = function(){
+            $state.go('menu');
+        };
     
-    ingredients.getIngredients().then(function(data) {
-    	$scope.Ingredients = data.data;
-    }) 
 });
